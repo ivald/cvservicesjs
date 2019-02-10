359667c66c2e729ebd5f8bb8b554a106a9e626f6
ivald79 <ivald79@gmail.com>
2019-02-10 00:04:57 -0500

    book and genre path were deleted form index.js

--- server/api/index.js
+++ server/api/index.js
@@ -4,7 +4,5 @@ module.exports = (app) => {
   app.use('/rest', require('./user/user.routes'));
   // app.use('/api/logins', require('./login/login.routes'));
   // app.use('/api/roles', require('./role/role.routes'));
-  // app.use('/api/books', require('./old_api/book/book.routes'));
-  // app.use('/api/genres', require('./old_api/genre/genre.routes'));
   // app.use('/api/auth', require('./auth/genre.routes'));
 }

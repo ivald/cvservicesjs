36a7fb69cea1137d8499fa4c3e917fb105f688ec
ivald79 <ivald79@gmail.com>
2019-02-09 23:53:56 -0500

    old_api and old_modules were deleted from project

--- server/api/old_api/book/book.routes.js
+++
@@ -1,8 +0,0 @@
-const controller = require('./book.controller');
-const express = require('express');
-const router = express.Router();
-
-router.get('/allBooks', controller.allBooks);
-router.post('/saveUserBook', controller.saveUserBook);
-
-module.exports = router;
--- server/api/old_api/user/user.routes.js
+++
@@ -1,9 +0,0 @@
-const controller = require('./user.controller');
-const auth = require('../../middleware/auth');
-const express = require('express');
-const router = express.Router();
-
-router.get('/me', auth, controller.currentUser);
-router.post('/', controller.addUser);
-
-module.exports = router;

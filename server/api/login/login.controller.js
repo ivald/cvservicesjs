d9cb440c2979251c9b9c6da0303c03dc066044ed
ivald79 <ivald79@gmail.com>
2019-05-02 21:58:30 -0400

    login validation

--- server/api/login/login.controller.js
+++ server/api/login/login.controller.js
@@ -1,7 +1,13 @@
 'use strict';
-const userController = require('../user/user.controller');
 
+const userController = require('../user/user.controller');
+const _ = require('lodash');
+const bcrypt = require('bcrypt');
 const models = require('../../models/db');
+const constant = require('../constant');
+
+let env       = process.env.NODE_ENV || 'development';
+let config    = require('../../../config/config.js')[env];
 
 let UserInfo = models.user_info;
 let Login = models.login;
@@ -31,8 +37,36 @@ exports.findAll = async (req, res) => {
 };
 
 exports.login = async (req, res) => {
-    const user = await userController.findUserByName(req.body.userName);
-    const token = UserInfo.generateAuthToken(user.userName);
-    user.token = token;
-    res.send(user);
+    let userParam = new UserInfo(_.pick(req.body, ['userName', 'password']));
+
+    if(constant.GUEST_USERNAME === userParam.userName) {
+        userParam.password = config.guest_password;
+    }
+
+    if (isNanOrNull(userParam.userName) || isNanOrNull(userParam.password)) {
+        return res.status(400).send('Please fill in username and password.');
+    }
+
+    const user = await userController.findLoginUserByName(userParam.userName);
+
+    if (isNanOrNull(user)) {
+        return res.status(400).send('User name not found.');
+    }
+
+    let result = await bcrypt.compare(userParam.password, user.password);
+
+    if(result) {
+        const token = UserInfo.generateAuthToken(user.userName);
+        user.token = token;
+        res.send(user);
+    } else {
+        return res.status(400).send('Invalid login. Please check your username and password.');
+    }
 };
+
+function isNanOrNull(element) {
+    if(element == null || element == "undefined" || element == undefined ||  element == "")
+        return true;
+    else
+        return false;
+}
